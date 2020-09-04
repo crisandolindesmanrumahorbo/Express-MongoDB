63aae2d2a94291ba8a77c23a26a6e63317c86c59
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2020-09-04 16:50:34 +0700

    add collection in student.model.js

--- src/api/students/student.model.js
+++ src/api/students/student.model.js
@@ -4,6 +4,6 @@ const studentSchema = new mongoose.Schema({
     name: {type: String, default: ''},
     place: {type: String},
     planet: {type: String, default: 'Earth'}
-});
+}, {collection: 'students'});
 
 module.exports = studentSchema;
